3f63626b2ab959068e693d61b744841b8b7f3ed1
anuwuth <anuwuth.ch@gmail.com>
2026-01-16 11:24:17 +0700

    update

--- src/practical/p02/index.ts
+++ src/practical/p02/index.ts
@@ -8,20 +8,19 @@ interface user {
   body: string;
 }
 
-export async function etPostsByUser() {
+export async function getPostsByUser(userId: number) {
   try {
     const response = await axios.get<user[]>('https://jsonplaceholder.typicode.com/posts');
     const posts = response.data;
 
     if (posts.length === 0) return [];
 
-    const firstPost = posts[0];
-    const secondPost = posts[1];
-
-    const result = [
-      { id: firstPost.id, title: firstPost.title },
-      { id: secondPost.id, title: secondPost.title }
-    ];
+    const result = posts
+      .filter(post => post.userId === userId)
+      .map(post => ({
+        id: post.id,
+        title: post.title
+      }));
 
     return result;
   } catch (error) {
